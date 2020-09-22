2214bb81635bb1fe3b92eeaf64409a17c269b4aa
rajeev <rajeev29five@gmail.com>
2020-09-22 11:38:45 +0530

    new audio files and corresponding enums added

--- app/src/main/java/com/anime/kyatchifurzu/AudioDetailsEnum.java
+++ app/src/main/java/com/anime/kyatchifurzu/AudioDetailsEnum.java
@@ -1,14 +1,18 @@
 package com.anime.kyatchifurzu;
 
-public enum AudioDetails {
+public enum AudioDetailsEnum {
 
+    DATTEBAYO("Dattebayo Naruto:Naruto"),
+    NARUTOKUN("Naruto-kun Hinata:Naruto"),
+    NIUHUHU("Niu-hu-hu Koro-sensei:Assassination classroom"),
+    TUTURU("Tuturu:Steins Gate"),
     YOOOOO("Yooooo:UNKNOWN"),
     YOOOOO_FULL("Yooooo Full:UNKNOWN"),
     YOYOI("Yoyoi Kumadori:One Piece");
 
     private String audioDetails;
 
-    AudioDetails(String audioDetails) {
+    AudioDetailsEnum(String audioDetails) {
         this.audioDetails = audioDetails;
     }
 
